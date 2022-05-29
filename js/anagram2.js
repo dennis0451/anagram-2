d7a1fb6e8de8527982f6a3e2b64abfa4639efebd
Dennis Corral <dennis.corral@outlook.com>
2022-05-29 19:42:38 -0400

    working solution

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,6 +1,6 @@
-// exports.anagramsFor = function(word, listOfWords) {
-    anagramsFor = function(word, listOfWords){
-        //sorted word in alpha order
+exports.anagramsFor = function(word, listOfWords) {
+    // anagramsFor = function(word, listOfWords){
+    //sorted word in alpha order
     let sortedWord = word.split('')
     sortedWord.sort(function (a,b){
         if(a < b){return -1}
@@ -8,24 +8,31 @@
         if(a == b){return 0}
     })
     sortedWord = sortedWord.join('')
-    // sorted list of words in alpha order
-    for(let i = 0; i < listOfWords.length; i++){
-        listOfWords[i] = listOfWords[i].split('')
-        listOfWords[i] = listOfWords[i].sort(function (a,b){
-            if(a < b){return -1}
-            if(a > b){return 1}
-            if(a == b){return 0}
-        })
-        listOfWords[i] = listOfWords[i].join('')
-    }
-    //compare strings
-    let anagramArray = []
-    for(let k = 0; k < listOfWords.length; k++){
-        if(sortedWord == listOfWords[k]){
-            anagramArray.push(listOfWords[k])
+    //made object with word as key and sorted word as value
+    let wordListObj ={}
+    for(let j = 0; j < listOfWords.length; j++){
+        if(!wordListObj.hasOwnProperty(listOfWords[j])){
+            wordListObj[listOfWords[j]] = listOfWords[j].split('').sort(function (a,b){
+                if(a < b){return -1}
+                if(a > b){return 1}
+                if(a == b){return 0}
+            }).join('')
         }
     }
-    return anagramArray
+    // console.log(wordListObj)
+    let wordListArr = Object.entries(wordListObj)
+    // console.log(wordListArr)
+
+    //compare sortedWord to possible anagram
+    let arrOfAnograms = []
+    for(let k = 0; k < wordListArr.length; k++){
+        if(wordListArr[k][1] === sortedWord){
+            arrOfAnograms.push(wordListArr[k][0])
+        }
+    }
+    // console.log(arrOfAnograms)
+    return arrOfAnograms
+
 };
 
-console.log(anagramsFor("saltier", ["cognac", "saltier", "realist", "retails"]))
+// console.log(anagramsFor("saltier", ["cognac", "saltier", "realist", "retails"]))
